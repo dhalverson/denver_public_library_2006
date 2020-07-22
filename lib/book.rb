c339ddf8331a684aff7f33852ed8808c6abf1415
Daniel Halverson <daniel.halverson@hotmail.com>
2020-07-22 12:38:33 -0600

    Add author class and test

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,19 @@
 class Book
 
+  attr_reader :title
   def initialize(args)
     @author_first_name = args[:author_first_name]
     @author_last_name = args[:author_last_name]
     @title = args[:title]
     @publication_date = args[:publication_date]
+  end
+
+  def author
+    "#{@author_first_name} #{@author_last_name}"
+  end
+
+  def publication_year
+    @publication_date.chars.last(4).join
   end
 
 end
