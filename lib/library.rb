a4643737182617d785a38f54c597f03cf0a2346e
Daniel Halverson <daniel.halverson@hotmail.com>
2020-07-22 13:19:08 -0600

    Add library add author

--- lib/library.rb
+++ lib/library.rb
@@ -1,7 +1,14 @@
 class Library
 
+  attr_reader :name,
+              :authors
   def initialize(name)
     @name = name
+    @authors = []
+  end
+
+  def add_author(author)
+    @authors << author
   end
 
 end
--- test/library_test.rb
+++ test/library_test.rb
@@ -18,4 +18,20 @@ class LibraryTest < Minitest::Test
     assert_equal "Denver Public Library", dpl.name
   end
 
+  def test_it_can_add_author
+    dpl = Library.new("Denver Public Library")
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    assert_equal [], dpl.authors
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+
+    
+  end
+
 end
